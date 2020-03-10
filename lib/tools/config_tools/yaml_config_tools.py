e22ef0f9b7445a5baeb2875488ee0c0579db39e9
dvegh <dvegh@gmail.com>
2020-03-10 10:17:25 +0100

    add config_tools.yaml_config_tools module

---
+++ lib/tools/config_tools/yaml_config_tools.py
@@ -0,0 +1,25 @@
+import yaml
+
+
+def find_value_with_key_dotted_path(yaml_object, key_dotted_path):
+    if key_dotted_path.find('.') == -1:
+        return yaml_object[key_dotted_path]
+    else:
+        pos = key_dotted_path.find('.')
+        yaml_object = yaml_object[key_dotted_path[:pos]]
+        key_dotted_path = key_dotted_path[pos + 1:]
+        return find_value_with_key_dotted_path(yaml_object, key_dotted_path)
+
+
+def resolve_double_curly_brackets_variables(file):
+    raw_yaml = yaml.load(open(file=file, mode='r'), Loader=yaml.FullLoader)
+    data = open(file=file, mode='r').read()
+
+    while True:
+        pos1, pos2 = data.find('{{ '), str(data).find(' }}')
+        if pos1 == -1:
+            break
+        data = data.replace('{{ ' + data[(pos1 + 3):(pos2 - 0)] + ' }}',
+                            find_value_with_key_dotted_path(raw_yaml, data[(pos1 + 3):(pos2 - 0)]))
+
+    return yaml.safe_load(data)
